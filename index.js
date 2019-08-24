8a06263a03b0ea8da73c63462ad9f74ac6a384d6
Miguel Bolivar <MBolivar360@gmail.com>
2019-08-24 13:40:28 -0400

    Using yargs, adding stress part and loggin both cascade and parallel

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const fs = require("fs");
 const { loadWithSharp, loadWithCanvas } = require("./utils");
+const yargs = require('yargs');
 const {
     PerformanceObserver
 } = require('perf_hooks');
 
 const imagesPath = "./images";
+
+const { cascade } = yargs
+    .option('parallel', {
+        alias: 'p',
+        describe: 'run the script in parallel, this is default behavior'
+    })
+    .option('cascade', {
+        alias: 'c',
+        describe: 'run the script in casacade, one after the other to measure individual performance on each image'
+    })
+    .option('stress', {
+        alias: 's',
+        describe: 'run the script with more tasks for each sharp and canvas, like creating a canvas and loading the image, or rotate the image and resize it'
+    })
+    .help()
+    .argv;
+
+async function processArrayWith(resultsArray, processFunction) {
+    for (const result in resultsArray) {
+        const [width, height] = resultsArray[result].replace(/\.jpg/gi, '').split('x');
+        await processFunction(`${imagesPath}/${resultsArray[result]}`, Number(width), Number(height))
+    }
+}
 
 fs.readdir(imagesPath, async (err, results) => {
     if (err) {
@@ -27,23 +51,34 @@ fs.readdir(imagesPath, async (err, results) => {
     });
     obs.observe({ entryTypes: ['measure'], buffered: false});
 
-    console.log(':: ---- Sharp ---- ::');
-    // Load images Sharp
-    const sharpArrayOfPromises = ordered.map(async (result) => {
-        const [width, height] = result.replace(/\.jpg/gi, '').split('x');
-        return loadWithSharp(`${imagesPath}/${result}`, Number(width), Number(height));
-    })
+    if (cascade) {
+        console.log(':: ---- Sharp ---- ::');
+        await processArrayWith(ordered, loadWithSharp);
+        console.log(':: --------------- ::');
 
-    const sharpResults = await Promise.all(sharpArrayOfPromises);
+        console.log(':: ---- Canvas ---- ::');
+        await processArrayWith(ordered, loadWithCanvas);
+        console.log(':: --------------- ::');
+    } else {
+        console.log(':: ---- Sharp ---- ::');
+        // Load images Sharp
+        const sharpArrayOfPromises = ordered.map(async (result) => {
+            const [width, height] = result.replace(/\.jpg/gi, '').split('x');
+            return loadWithSharp(`${imagesPath}/${result}`, Number(width), Number(height));
+        })
 
-    console.log(':: ---- ---- ---- ::');
-    console.log(':: ---- Canvas ---- ::');
-    // Load images Canvas
-    const canvasArrayOfPromises = ordered.map(async (result) => {
-        const [width, height] = result.replace(/\.jpg/gi, '').split('x');
-        return loadWithCanvas(`${imagesPath}/${result}`, Number(width), Number(height));
-    });
-    
-    const canvasResults = await Promise.all(canvasArrayOfPromises);
-    console.log(':: ---- ---- ---- ::');
+        const sharpResults = await Promise.all(sharpArrayOfPromises);
+
+        console.log(':: ---- ---- ---- ::');
+        console.log(':: ---- Canvas ---- ::');
+        // Load images Canvas
+        const canvasArrayOfPromises = ordered.map(async (result) => {
+            const [width, height] = result.replace(/\.jpg/gi, '').split('x');
+            return loadWithCanvas(`${imagesPath}/${result}`, Number(width), Number(height));
+        });
+        
+        const canvasResults = await Promise.all(canvasArrayOfPromises);
+        console.log(':: ---- ---- ---- ::');
+    }
+
 });
--- utils.js
+++ utils.js
@@ -1,31 +1,44 @@
 const sharp = require("sharp");
-const { loadImage } = require("canvas");
+const { loadImage, createCanvas } = require("canvas");
+const chalk = require('chalk');
 const {
     performance
 } = require('perf_hooks');
 
 function getMemoryUsage() {
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`The script uses approximately ${used} MB`);
-    console.log(':: ------------------------ ::');
+    console.log(chalk.blue(`The script uses approximately ${used} MB`));
+    console.log(chalk.blue(':: ------------------------ ::'));
 }
 
-async function loadWithSharp(path) {
+async function loadWithSharp(path, stress=false) {
     const perfTag = `Sharp-${path}`;
     performance.mark(perfTag + '-init');
-    const sharppedResult = await sharp(path).toBuffer();
+    const pipeline = sharp(path);
+    // Only stress
+    if (stress) {
+        pipeline.rotate().resize(200);
+    }
+    await pipeline.toBuffer();
     performance.mark(perfTag + '-end');
-    console.log(`Image ${path} :: Memory consumption`);
+    console.log(chalk.green(`Image ${path} :: Memory consumption`));
     performance.measure(perfTag, perfTag + "-init", perfTag + "-end")
     getMemoryUsage();
 }
 
-async function loadWithCanvas(path,) {
+async function loadWithCanvas(path, width, height, stress=false) {
     const perfTag = `Canvas-${path}`;
     performance.mark(perfTag + '-init');
-    const canvasedResult = await loadImage(path);
+    const canvasedResult = await loadImage(path, width, height);
+    // Only stress
+    if (stress) {
+        const canvas = createCanvas(width, height);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(canvasedResult, 0, 0, width, height);
+        canvas.toDataURL();
+    }
     performance.mark(perfTag + '-end');
-    console.log(`Image ${path} :: Memory consumption`);
+    console.log(chalk.green(`Image ${path} :: Memory consumption`));
     performance.measure(perfTag, perfTag + "-init", perfTag + "-end")
     getMemoryUsage();
 }
